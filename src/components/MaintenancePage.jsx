e461563ff74bb1cab432ae06d09461b3719f2317
kamlali9990-ui <kamlali9990@gmail.com>
2026-06-08 05:43:55 +0300

    إضافة لعبة الثعبان لصفحة الصيانة التي يراها الزوار

--- src/components/MaintenancePage.jsx
+++ src/components/MaintenancePage.jsx
@@ -1,4 +1,6 @@
 import { BASE } from '../utils/constants';
+import SnakeGame from './SnakeGame';
+
 export default function MaintenancePage() {
   return (
     <div className="maintenance-page">
@@ -6,6 +8,9 @@ export default function MaintenancePage() {
       <div className="maintenance-icon">🔧</div>
       <h1>الموقع قيد الصيانة</h1>
       <p>نعمل على تحسين تجربتك. سنعود قريباً!</p>
+      <div style={{ marginTop: '1.5rem' }}>
+        <SnakeGame />
+      </div>
     </div>
   );
 }
